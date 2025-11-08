3848305172e106a354b0b4c869424a1ef42836b3
KshitijPatil08 <138120174+KshitijPatil08@users.noreply.github.com>
2025-11-08 20:16:19 +0530

    Update page.tsx

--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { DeviceManagement } from "@/components/device-management"
-import { RealTimeAlerts } from "@/components/real-time-alerts"
+import  RealTimeAlerts  from "@/components/real-time-alerts"
 
 export default function DevicesPage() {
   return (
